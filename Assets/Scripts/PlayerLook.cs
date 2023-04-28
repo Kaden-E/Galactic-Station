dee04d50c0a9d540ae1a69fec400c9267bb76210
Kadnツ <kaden.ellis2005@gmail.com>
2023-04-28 10:27:16 +0100

    Update PlayerLook.cs
    Fixed hypersensitive cam and clamping issue

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerLook : MonoBehaviour
 {
-    public Camera _cam;
+    [FormerlySerializedAs("_cam")] public Camera cam;
     public float xRotation = 0f;
 
     public float xSens = 30f;
@@ -14,14 +15,14 @@ public class PlayerLook : MonoBehaviour
     public void ProcessLook(Vector2 input)
     {
         var mouseX = input.x;
-        var mouseY = input.y;
+        var mouseY = input.y; 
 
         xRotation -= (mouseY * Time.deltaTime) * ySens;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         
-        _cam.transform.localRotation = quaternion.Euler(xRotation, 0, 0);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         
-        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime)*xSens);
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime * xSens));
     }
     
     
