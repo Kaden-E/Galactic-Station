b2da031d96e655333851bd435e973443963a573d
Kadnツ <kaden.ellis2005@gmail.com>
2023-05-02 21:13:34 +0100

    Basic state machine

---
+++ Assets/Scripts/Enemies and AI/ChaseState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : State
+{
+    public AttackState attackState;
+    public bool inRange;
+    
+    
+    public override State RunCurrentState()
+    {
+        if (inRange)
+        {
+            return attackState;
+        }
+        else
+            return this;
+    }
+}
